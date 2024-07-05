77d7477e2c4066cc0902e14a820f7caa29868d57
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 10:23:28 +0000

    conversion done

---
+++ 01_Basic/TypeConversion.js
@@ -0,0 +1,26 @@
+let a = "33a"  //string
+let numberA = Number(a)
+// console.log(numberA);
+// console.log(typeof numberA);
+
+
+/*
+"33"=> 33
+"33a" => NaN not a number
+*/
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+//console.log( booleanLoggedIn);
+
+/*
+1=> true
+0=>false
+"aa"=>true
+""=>false
+*/
+
+
+let p = null
+let stringNumber = String(p)
+console.log(stringNumber);
