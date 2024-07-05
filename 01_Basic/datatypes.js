2a00ca7a784e2c72420cf6ea8556cae8c7fb4641
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 10:09:39 +0000

    done

---
+++ 01_Basic/datatypes.js
@@ -0,0 +1,16 @@
+let Name="Shivkumar"
+let age = 21
+let isLoggedIn = false
+
+/*
+string => ""
+number => 2^53
+boolean
+null =>standalone value/object
+object
+symbol => for unique
+undefined 
+*/
+
+
+console.log(typeof Name)
