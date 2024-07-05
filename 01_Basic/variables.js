0f8821ba559554018383a7f12db9bd009dcf0825
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 09:56:22 +0000

    Commit config done

---
+++ 01_Basic/variables.js
@@ -0,0 +1,19 @@
+const accountId = 121
+let accountEmail = "shivkumardhabade@gmail.com"
+var accountPassword = "12345"
+accountCity = "solapur"
+let accountState
+accountPassword = "111"
+
+//accountId = 212 not possible because it is constant
+
+
+/*
+do not use var because it gives scope problems 
+
+*/
+
+
+//console.log(accountEmail)   // we can see one variable at a time
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
