1481ec54b7a026d877da2c932e8b8869c7129df4
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 12:44:32 +0000

    strings session done

---
+++ 01_Basic/strings.js
@@ -0,0 +1,23 @@
+const myName = "shiv kumar"
+const age = 21
+
+//console.log(`My Name Is ${myName} And My Age is ${age}`)
+
+// console.log(myName.length)
+
+// console.log(myName.toUpperCase())
+
+//console.log(myName.trim());
+
+// console.log(myName.substring(0,4))
+
+// console.log(myName.split(" "))
+
+// console.log(myName.toLowerCase())
+
+const MyGame = new String("Dhabade")
+// console.log(myName+" " +MyGame)
+
+// console.log(myName.replace(" ",""))
+
+console.log(myName.includes('kumar'))
