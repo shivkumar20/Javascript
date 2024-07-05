3178fcb9d8cb824252003dd8891efe7082138444
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 17:24:57 +0000

    date and time done

---
+++ 01_Basic/Date_and_Time.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+let myNewDate = new Date(2023,0,1,10,56,10)
+// console.log(myNewDate.toLocaleString())
+
+// console.log(Date.now());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+
+
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getSeconds());
+
+
+
+
+
+
+
+
