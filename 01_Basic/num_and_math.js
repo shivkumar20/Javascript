c451465691fdedbe0095c7b5ecb5b1a10085138a
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-05 17:02:07 +0000

    maths commited

---
+++ 01_Basic/num_and_math.js
@@ -0,0 +1,19 @@
+const num = new Number(10000)
+// console.log(num)
+// console.log(num.toLocaleString('en-IN'))
+// console.log(num.toFixed(2))
+// console.log(num.toString())
+// console.log(Number.MAX_VALUE)
+// console.log((Number.MIN_VALUE));
+
+
+// console.log(Math.min(3,5,4,1));
+// console.log(Math.max(3,5,4,1));
+// console.log(Math.sqrt(4))
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+
+console.log(Math.random());
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+min)
