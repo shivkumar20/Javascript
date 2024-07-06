1074cfaf7d1e829cc0d35c435ea4bee2bf3a0fed
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-06 06:45:29 +0000

    array part 2 done

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,39 @@
+const n1 = [1,2,3]
+const n2 = [4,5,6]
+// n1.push(n2)
+//console.log(n1);  // array is pushed on to the another array
+
+
+// console.log(n1[3][2]);
+
+// const n3 = n1.concat(n2)
+// console.log(n3);
+
+
+// const n4 = [1,2,3,[4,5,6],7,[8,[9,0]]]
+// console.log(n4);
+
+// const n5 = n4.flat(Infinity)
+// console.log(n5);
+
+// const n6 = [...n1,...n2]
+// console.log(n6);
+
+
+// let a = 10
+// let b = 20
+// let c = 30
+
+// let VarArr = Array.of(a,b,c)
+// console.log(VarArr);
+
+
+// let str = "shivkumar"
+// let strArr = Array.from(str)
+// console.log(str);
+// console.log(strArr);
+
+
+let newArr = [1,2,3]
+let newStr = "shiv"
+console.log(Array.isArray(newStr))
