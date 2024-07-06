e413beb6e6d8be1facdf5e97fc156f254653cd8e
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-06 06:21:02 +0000

    array session done

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,32 @@
+const myArr = [1,2,3,4,5,6]
+// console.log(myArr);
+
+const newArr = new Array("shiv","kumar")
+// console.log(newArr);
+
+//methods
+
+// console.log(newArr.length);
+
+// myArr.push(7)
+
+// myArr.pop()
+
+// myArr.unshift(0)
+
+// myArr.shift(0)
+
+
+
+// console.log("A " + myArr);
+// const N1 = myArr.slice(2,4)
+// console.log(N1);
+
+// console.log("B " + myArr);
+// const N2 = myArr.splice(2,3)
+// console.log(N2);
+
+
+// console.log(myArr.includes(4))
+
+console.log(myArr.indexOf(5));
