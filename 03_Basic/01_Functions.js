d17ceefda15af4eac06fce7a60d77ec83ed455a8
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-06 14:53:33 +0000

    functions part one done

---
+++ 03_Basic/01_Functions.js
@@ -0,0 +1,24 @@
+function myFun() {
+    console.log("Hello From My function");
+}
+
+// myFun()
+
+function myNewFuntion(name = "kumar") {
+  console.log(`hello ${name}`);  
+}
+
+// myNewFuntion("shiv")
+// myNewFuntion()
+
+
+function Addition(n1,n2) {
+    let result = n1+n2
+    return result
+}
+
+// let result = Addition(10,30)
+// console.log("Addition ",result);
+
+
+console.log(Addition(2,2));
