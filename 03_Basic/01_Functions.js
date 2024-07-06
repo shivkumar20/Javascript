502d7389a081eca3cf3cdcf5ae48f48ddc1acf85
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-06 15:07:15 +0000

    functions done

--- 03_Basic/01_Functions.js
+++ 03_Basic/01_Functions.js
@@ -21,4 +21,50 @@ function Addition(n1,n2) {
 // console.log("Addition ",result);
 
 
-console.log(Addition(2,2));
+// console.log(Addition(2,2));
+
+
+
+const user = {
+    username:"shivkumar",
+    age:21
+}
+
+function getObject(obj) {
+    console.log(`hello ${obj.username} from the object user`);
+}
+
+// getObject(user)
+
+// getObject({
+//     username:"vijay",
+//     age:26
+// })
+
+
+let myArr = [1,2,3,4]
+function getArray(arr) {
+    return arr[2]
+}
+
+// console.log(getArray(myArr))
+// console.log(getArray([10,20,30,40]))
+
+
+
+// function getAnyNumberParameter(...scores){
+//     return scores
+// }
+
+
+// console.log(getAnyNumberParameter(1000,2000,3000,4000))
+
+
+
+
+function getAnyNumberParameter(n1,n2,...scores){
+    return scores
+}
+
+
+console.log(getAnyNumberParameter(1000,2000,3000,4000))
