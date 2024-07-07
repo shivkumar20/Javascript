30f3d20130bc8ca14ed655e80ec8fedbc5c86f92
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-07 11:32:12 +0000

    done changes

---
+++ 04_ControlFlow/first.js
@@ -0,0 +1,11 @@
+// if(true){
+//     console.log("hello");
+
+// }
+
+let b =  null ?? 30
+
+console.log(b);
+
+let a = 100;
+(a>10)? console.log("a is greater than 10") : console.log("nit greater than 10");
---
+++ 04_ControlFlow/loops.js
@@ -0,0 +1,57 @@
+const myArr = ["a","b","c","d"]
+
+// for (const i of myArr) {
+//     console.log(i);
+// }
+
+
+const user = {
+    username : "shiv",
+    age:21
+}
+
+// for (const iterator of user) {
+//     console.log(iterator);                             //cant used with the object
+// }
+
+
+// for (const key in user) {
+//   console.log(`the key is ${key} and value is ${user[key]}`);
+// }
+
+
+// for (const key in myArr) {
+//    console.log(myArr[key]);
+// }
+
+
+
+// myArr.forEach((item) =>{
+//     console.log(item);
+// }) 
+
+
+
+// myArr.forEach( function (item) {
+//     console.log(item);
+// })
+
+
+
+let map = new Map()
+map.set("IN", "INDIA")
+map.set("MH", "MAHARASHTRA")
+map.set("PN", "PUNE")
+
+
+// for (const iterator of map) {
+//  console.log(iterator);          //prints the each record as array 
+// }
+
+
+
+// for (const [key,value] of map) {
+//     console.log(`key : ${key} value : ${value}`);
+// }
+
+
