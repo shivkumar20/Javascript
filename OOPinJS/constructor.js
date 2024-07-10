a2f8cbb6f7ea257eca939a98bbc0c48d4d1d165a
Shivkumar Dhabade <47687386+shivkumar20@users.noreply.github.com>
2024-07-10 09:37:45 +0000

    OOP done

---
+++ OOPinJS/constructor.js
@@ -0,0 +1,25 @@
+function User (name,age,marks){
+    this.name = name
+    this.age = age
+    this.marks = marks
+    // return this    // can automatically return
+}
+
+
+
+// const userOne = User('Shiv',21,90)
+// const userTwo = User('Kumar',22,100)   //this will override the values set by userOne
+
+
+
+// so to avoid this use new
+
+const userOne =new User('Shiv',21,90)
+const userTwo =new User('Kumar',22,100)
+const user = ''
+console.log(userOne);
+console.log(userTwo);
+
+
+
+console.log( user instanceof User);  //to check whether the object or not 
---
+++ OOPinJS/objectLiteral.js
@@ -0,0 +1,9 @@
+const user = {
+    username: "Shivkumar",
+    age: 21
+}
+
+
+
+console.log(user.username);
+console.log(user["age"]);
